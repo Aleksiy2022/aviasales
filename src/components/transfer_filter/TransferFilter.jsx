f06a0ab3ad22552366177b1aefce96d6d939b47b
Aleksey Kuznetsov <kuznetcov.alekcey@icloud.com>
2025-04-07 17:22:10 +0300

    Added styles for components and added a transfer filter.

--- src/components/transfer_filter/TransferFilter.jsx
+++ src/components/transfer_filter/TransferFilter.jsx
@@ -1,41 +1,65 @@
-import { Checkbox, Flex } from 'antd'
 import { useState } from 'react'
 
 import classes from './transfer_filter.module.scss'
 
-const transferOptions = ['Без пересадок', '1 пересадка', '2 пересадки', '3 пересадки']
-const defaultCheckedList = []
+const transferOptionLabels = [
+  { name: 'all', value: 'Все' },
+  { name: 'without', value: 'Без пересадок' },
+  { name: 'one', value: '1 пересадка' },
+  { name: 'two', value: '2 пересадки' },
+  { name: 'three', value: '3 пересадки' },
+]
 
 export default function TransferFilter() {
-  const [checkedList, setCheckedList] = useState(defaultCheckedList)
-  const checkAll = transferOptions.length === checkedList.length
-  const indeterminate = checkedList.length > 0 && checkedList.length < transferOptions.length
+  const [checkedStatus, setCheckedStatus] = useState(
+    transferOptionLabels.map((item) => {
+      return {
+        name: item.name,
+        checked: false,
+      }
+    })
+  )
 
-  function handleChange(list) {
-    setCheckedList(list)
+  function handleChange(evt) {
+    let allInputChecked = null
+    if (evt.target.name === 'all') {
+      allInputChecked = checkedStatus.find((item) => item.name === 'all').checked
+    }
+    const newCheckedStatus = checkedStatus.map((item) => {
+      if (evt.target.name === 'all') {
+        return { ...item, checked: !allInputChecked }
+      }
+      if (evt.target.name === item.name) {
+        return { ...item, checked: !item.checked }
+      }
+      return item
+    })
+    setCheckedStatus(newCheckedStatus)
   }
 
-  function handleCheckAllChange(e) {
-    setCheckedList(e.target.checked ? transferOptions : [])
-  }
+  const transferOptions = transferOptionLabels.map((label, index) => {
+    const currentInputItem = checkedStatus.find((item) => item.name === label.name)
+    return (
+      <li key={index}>
+        <label className={classes['transfer-filter_label']}>
+          <input
+            name={label.name}
+            type="checkbox"
+            className={classes['transfer-filter_checkbox']}
+            checked={currentInputItem.checked}
+            onChange={handleChange}
+          />
+          <span className={classes['transfer-filter_custom-checkbox']}></span>
+          <span>{label.value}</span>
+        </label>
+      </li>
+    )
+  })
 
   return (
-    <div className={classes['transfer-filter']}>
+    <form className={classes['transfer-filter']}>
       <span className={classes['transfer-filter_title']}>КОЛИЧЕСТВО ПЕРЕСАДОК</span>
-      <Checkbox indeterminate={indeterminate} onChange={handleCheckAllChange} checked={checkAll}>
-        Все
-      </Checkbox>
-      <Checkbox.Group
-        className={classes['checkbox-group']}
-        options={transferOptions}
-        value={checkedList}
-        onChange={handleChange}
-      >
-        <Checkbox value="Not transfer">Без пересадок</Checkbox>
-        <Checkbox value="1 transfer">1 пересадка</Checkbox>
-        <Checkbox value="2 transfer">2 пересадки</Checkbox>
-        <Checkbox value="3 transfer">3 пересадки</Checkbox>
-      </Checkbox.Group>
-    </div>
+      <ul className={`${classes['list-reset']} ${classes['transfer-filter_list']}`}>{transferOptions}</ul>
+    </form>
   )
 }
