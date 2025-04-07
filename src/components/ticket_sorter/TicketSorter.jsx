e5f79d2aed7b61419e5b677c6a036b87a9a9f655
kashmir <kuznetcov.alekcey@icloud.com>
2025-04-07 23:53:51 +0300

    Added handle button click.

--- src/components/ticket_sorter/TicketSorter.jsx
+++ src/components/ticket_sorter/TicketSorter.jsx
@@ -11,10 +11,26 @@ const initialState = [
 export default function TicketSorter() {
   const [buttonsData, setButtonsData] = useState(initialState)
 
+  function handleClick(evt, value) {
+    evt.preventDefault()
+    const newButtonsData = buttonsData.map((button) => {
+      if (button.value === value) {
+        return { ...button, active: true }
+      }
+      return { ...button, active: false }
+    })
+    setButtonsData(newButtonsData)
+  }
+
   const buttons = buttonsData.map((item, index) => {
     return (
       <li key={index} className={`${classes['ticket-sorter__item']} ${item.active ? classes['active'] : null}`}>
-        <button className={`${classes['btn-reset']} ${classes['ticket-sorter__btn']}`}>{item.text}</button>
+        <button
+          className={`${classes['btn-reset']} ${classes['ticket-sorter__btn']}`}
+          onClick={(evt) => handleClick(evt, item.value)}
+        >
+          {item.text}
+        </button>
       </li>
     )
   })
