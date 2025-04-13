1cde1486864c4bc8d8d66bcc109e749c02643a82
Aleksey Kuznetsov <kuznetcov.alekcey@icloud.com>
2025-04-13 20:05:41 +0300

    Added store and slices.

--- src/components/app/store.js
+++ src/components/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore, combineSlices } from '@reduxjs/toolkit'
 import transferFilterSlice from '../transfer_filter/transferFilterSlice.js'
+import ticketSorterSlice from '../ticket_sorter/ticketSorterSlice.js'
 
-const rootReducer = combineSlices(transferFilterSlice)
+const rootReducer = combineSlices(transferFilterSlice, ticketSorterSlice)
 
 export const store = configureStore({
   reducer: rootReducer,
--- src/components/ticket_sorter/ticketSorterSlice.js
+++ src/components/ticket_sorter/ticketSorterSlice.js
@@ -1,1 +1,29 @@
 import { createAppSlice } from '../app/createAppSlice.js'
+
+const initialState = {
+  options: [
+    { value: 'cheapest', label: 'Самый дешевый' },
+    { value: 'fastest', label: 'Самый быстрый' },
+    { value: 'optimal', label: 'Оптимальный' },
+  ],
+  currentSortedValue: 'cheapest'
+}
+
+const ticketSorterSlice = createAppSlice({
+  name: 'ticketSorter',
+  initialState,
+  reducers: (create) => ({
+    setCurrentSortedValue: create.reducer((state, actions) => {
+      const { value } = actions.payload;
+      state.currentSortedValue = value;
+    })
+  }),
+  selectors: {
+    selectOptionTicketSorter: (state) => state.options,
+    selectCurrentSortedValue: (state) => state.currentSortedValue,
+  }
+})
+
+export default ticketSorterSlice
+export const { setCurrentSortedValue } = ticketSorterSlice.actions
+export const { selectOptionTicketSorter, selectCurrentSortedValue } = ticketSorterSlice.selectors
